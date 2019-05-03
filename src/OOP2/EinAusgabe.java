d2ef5e6150ee178cb7771693aea979047c314710
amyjosi <49399582+amyjosi@users.noreply.github.com>
2019-05-03 21:29:06 +0200

    update EinAusgabe

--- src/OOP2/EinAusgabe.java
+++ src/OOP2/EinAusgabe.java
@@ -5,33 +5,63 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.List;
 
 /**
- * Josis
+ * @author Josis
  */
 public class EinAusgabe {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static String textEingabe()
     {
-        throw new NotImplementedException();
+        return Scanner.nextLine();
     }
 
     public static void textAusgabe()
     {
-        throw new NotImplementedException();
+        System.out.println(textausgabe);
     }
 
     public static int intAbfrage(String frage)
     {
-        throw new NotImplementedException();
+        textAusgabe(frage);
+        String eingabe = textEingabe().trim().toLowerCase().replaceAll("[\\D]", "");
+        int zahl;
+        boolean weiterfragen = true;
+        while (weiterfragen) {
+            try {
+                zahl = Integer.parseInt(eingabe);
+                weiterfragen = false;
+            } catch (NumberFormatException nf) {
+                System.out.println("Eingegebener Wert ist keine Zahl! Bitte erneut eingeben.");
+                weiterfragen = true;
+            }
+        } catch (Exception e) {
+        System.out.println("Eingegebener Wert ist keine Zahl! Bitte erneut eingeben.");
+        weiterfragen = true;
+        }
+
+       return zahl;
     }
 
     public static Vector2 positionsAbfrage(String frage)
     {
-        throw new NotImplementedException();
+        //TODO Bitte auf Spielfeldgröße achten!
+        textAusgabe(frage);
+        int x = intAbfrage("Bitte x eingeben!");
+        int y = intAbfrage("Bitte Y eingeben!");
+        Vector2 vector = new Vector2(x,y);
+        return vector;
     }
 
     public static int auswahlAbfrage(String frage, List<String> möglichkeiten)
     {
-        throw new NotImplementedException();
+        textAusgabe(frage);
+        int i = 0;
+        for (String s : möglichkeiten) {
+            textAusgabe(i + ": " + s);
+            i++;
+        }
+        int auswahl = intAbfrage("Bitte die gewählte Nummer eingeben!")
+        return auswahl;
     }
 
 }
